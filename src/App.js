4d81133cf58bfa0d695859c8bf5f2f3283a53843
iamsuzeet <iamsuzeet@gmail.com>
2019-09-29 16:53:17 +0545

    app component structured

--- src/App.js
+++ src/App.js
@@ -5,12 +5,55 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 class App extends Component {
+  state = {
+    items: [{ id: 1, title: "wake up" }, { id: 2, title: "eat breakfast" }],
+    id: uuid(),
+    item: "",
+    editItem: false
+  };
+
+  handleChange = e => {
+    console.log("handle change");
+  };
+
+  handleSubmit = e => {
+    console.log("handle Submit");
+  };
+
+  handleClearList = () => {
+    console.log("handle clearlist");
+  };
+
+  handleEdit = id => {
+    console.log(id);
+  };
+
+  handleDelete = id => {
+    console.log(id);
+  };
+
   render() {
+    const { items, item, editItem } = this.state;
+
     return (
       <div className="container">
         <div className="row">
-          <TodoInput />
-          <TodoList />
+          <div className="col-10 mx-auto col-md-8 mt-5">
+            <h3 className="text-capitalize text-center">
+              <TodoInput
+                item={item}
+                handleChange={this.handleChange}
+                handleSubmit={this.handleSubmit}
+                editItem={editItem}
+              />
+              <TodoList
+                items={items}
+                handleEdit={this.handleEdit}
+                handleDelete={this.handleDelete}
+                handleClearList={this.handleClearList}
+              />
+            </h3>
+          </div>
         </div>
       </div>
     );
